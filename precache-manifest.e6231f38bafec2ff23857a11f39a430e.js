99aabf9b40a67c89d2ff357359a063fb2eb5c37b
Nazar <nazshved@gmail.com>
2020-09-23 14:24:07 +0300

    Updates

---
+++ precache-manifest.e6231f38bafec2ff23857a11f39a430e.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81ebb04191fda424c57f37b4bc1883a0",
+    "url": "/react_2/index.html"
+  },
+  {
+    "revision": "072be6fbf3094d675c70",
+    "url": "/react_2/static/css/main.91e92042.chunk.css"
+  },
+  {
+    "revision": "235cac538cf1cc2c19fd",
+    "url": "/react_2/static/js/2.b511e22c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react_2/static/js/2.b511e22c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "072be6fbf3094d675c70",
+    "url": "/react_2/static/js/main.d7f2879d.chunk.js"
+  },
+  {
+    "revision": "1c7e300880b78ac7f767",
+    "url": "/react_2/static/js/runtime-main.899744e7.js"
+  },
+  {
+    "revision": "71ee171942959cc738c95300424ef3e0",
+    "url": "/react_2/static/media/anonymous.71ee1719.svg"
+  },
+  {
+    "revision": "58b5d0bb60e886d9cc7ab261d8996b4c",
+    "url": "/react_2/static/media/call.58b5d0bb.svg"
+  },
+  {
+    "revision": "5c87994c964d115c484b909be298743b",
+    "url": "/react_2/static/media/female.5c87994c.svg"
+  },
+  {
+    "revision": "075829956d01b4f9d072ca02aa84e2f2",
+    "url": "/react_2/static/media/mars.07582995.svg"
+  }
+]);
